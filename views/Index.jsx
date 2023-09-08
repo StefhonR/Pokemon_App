fb20203a1de59ba1521685bae9cccd5b1598b466
Stefhon Roach <kingroach@Stefhons-MBP.lan>
2023-09-07 20:48:50 -0400

    Added dynamic anchor tags to index.jsx

--- views/Index.jsx
+++ views/Index.jsx
@@ -16,7 +16,9 @@ class Index extends React.Component {
                     pokemon.map((pokemon, i) => {
                         return (
                             <li key={i}>
+                                <a href={`/pokemon/${i}`}>
                                 {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                                </a>
                             </li>
                         )
                     })
